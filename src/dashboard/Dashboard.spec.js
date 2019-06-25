7d4bbc1a0d47cb4c9dd9d78e7119a48270e81ccf
John O'Keefe <jokeefe@weegolobal.com>
2019-06-25 19:23:30 -0400

    fixed spacing

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,20 +1,18 @@
 // Test away
-import 'jest-dom/extend-expect';
-import React from 'react';
-import { render } from 'react-testing-library';
-import Controls from '../controls/Controls';
-import Display from '../display/Display';
-import Dashboard from './Dashboard';
+import 'jest-dom/extend-expect'
+import React from 'react'
+import { render } from 'react-testing-library'
+import Controls from '../controls/Controls'
+import Display from '../display/Display'
+import Dashboard from './Dashboard'
 
 it('should render Dashboard', () => {
     render(<Dashboard />)
 })
 
-
 it('should render Display', () => {
     render(<Display />)
 })
-
 
 it('should render Controls', () => {
     render(<Controls />)
@@ -28,4 +26,3 @@ it('should render buttons', () => {
     getByText(/lock gate/i)
     getByText(/close gate/i)
 })
-
